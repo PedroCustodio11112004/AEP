88d351f2774f3f4a79e1bf0893556a79a5da8a19
Pedro Henrique Ribeiro Custódio <pedrohenrique11112004@hotmail.com>
2025-06-15 16:10:22 -0300

    feat(ProjetoAEP): Finalizado

--- BackEndAEP/src/main/java/com/example/BackEndAEP/Controller/ReclamacaoController.java
+++ BackEndAEP/src/main/java/com/example/BackEndAEP/Controller/ReclamacaoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ReclamacaoController {
@@ -21,8 +22,9 @@ public class ReclamacaoController {
     }
 
     @PostMapping("/registrar")
-    public String registerReclamacao(Reclamacao reclamacao) {
+    public String registerReclamacao(Reclamacao reclamacao, RedirectAttributes redirectAttributes) {
         reclamacaoRepository.save(reclamacao);
+        redirectAttributes.addFlashAttribute("mensagem", "Reclamação registrada com sucesso!");
         return "redirect:/listar";
     }
 
